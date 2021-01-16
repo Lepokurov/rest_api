f52d3f0c06f7b91486bf73c0581a4ff9c6e16a24
Andrey Lepokurov <endreyva@gmail.com>
2021-01-16 23:32:18 +0700

    Update

--- db_connector.py
+++ db_connector.py
@@ -15,16 +15,18 @@ class User(db.Model):
 
     def del_from_db(self):
         db.session.delete(self)
-        db.session.commit()
+        upd()
 
     def add_to_db(self):
         db.session.add(self)
-        db.session.commit()
+        upd()
 
     def upd_db(self):
-        db.session.commit()
+        upd()
 
     def __repr__(self):
         return f"{self.username} - {self.email} - {self.rating}"
 
 
+def upd():
+    db.session.commit()
